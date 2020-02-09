0ad702b14c98a084ed2481a9c3510e0ab3cb2957
Scott Johnson <scottyjohnson002@gmail.com>
2020-02-09 12:04:34 -0500

    change all instance of your project to week-5-test

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="week-5-test"
+    version="0.0.1",
+    author="Scott Johnson",
+    author_email="ScottyJohnson002@gmail.com",
+    url="https://github.com/ScottJohnson02/week-5-test",
+    description="basic set-up for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    extras_require=[],
+    tests_require=['pytest'],
+    python_requires='>=3.6',
+)
+
